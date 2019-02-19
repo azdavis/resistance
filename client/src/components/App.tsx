1ea40ee9fc5f501d86d016eb3a295492868f1172
Ariel Davis <ariel.z.davis@icloud.com>
2019-02-19 15:37:25 +0900

    Shorten

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -12,7 +12,7 @@ export default (): JSX.Element => {
   const [send, setSend] = useState<Send | null>(null);
   useEffect(() => {
     const ws = new WebSocket("wss://echo.websocket.org");
-    ws.onopen = () => setSend(() => (msg: Msg) => ws.send(JSON.stringify(msg)));
+    ws.onopen = () => setSend(() => (m: Msg) => ws.send(JSON.stringify(m)));
     ws.onmessage = e => d(JSON.parse(e.data));
     ws.onclose = () => d({ t: "closed" });
     return ws.close.bind(ws);
