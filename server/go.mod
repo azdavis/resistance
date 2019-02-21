761080bff59f3f5bf9271b0bcb46a66742a54910
Ariel Davis <ariel.z.davis@icloud.com>
2019-02-22 00:57:12 +0900

    Add echo server

--- server/go.mod
+++ server/go.mod
@@ -1,1 +1,3 @@
 module github.com/azdavis/resistance
+
+require github.com/gorilla/websocket v1.4.0
--- server/main.go
+++ server/main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+func unsafeAllowAny(r *http.Request) bool {
+	return true
+}
+
+var up = websocket.Upgrader{CheckOrigin: unsafeAllowAny}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	c, err := up.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, m, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", m)
+		err = c.WriteMessage(mt, m)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
 func main() {
-	fmt.Println("hello")
+	log.Println("start")
+	http.HandleFunc("/", serveWs)
+	http.ListenAndServe(":8080", nil)
 }
