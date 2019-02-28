645714ef6d1ce6be2a05eee53e91f8ec3966dcdf
Ariel Davis <ariel.z.davis@icloud.com>
2019-02-28 15:13:55 +0900

    Rm redundant addClient / rmClient helpers

--- server/lobby.go
+++ server/lobby.go
@@ -17,31 +17,20 @@ func newLobby() *lobby {
 }
 
 func (h *lobby) run() {
-	clients := make(map[ID]*client)
 	cm := newClientMap()
-	addClient := func(cl *client) {
-		log.Println("addClient", cl.id)
-		clients[cl.id] = cl
-		cm.add(cl)
-	}
-	rmClient := func(id ID) {
-		log.Println("rmClient", id)
-		delete(clients, id)
-		cm.rm(id)
-	}
 	for {
 		select {
 		case cl := <-h.clientCh:
-			addClient(cl)
+			cm.add(cl)
 		case idAc := <-cm.c:
 			id := idAc.ID
 			switch ac := idAc.Action.(type) {
 			case Close:
-				rmClient(id)
+				cm.rm(id)
 			case NameChoose:
 				log.Println("NameChoose", id, ac.Name)
-				clients[id].name = ac.Name
-				clients[id].send <- PartyChoosing{
+				cm.m[id].name = ac.Name
+				cm.m[id].send <- PartyChoosing{
 					Name:    ac.Name,
 					Parties: []string{},
 				}
