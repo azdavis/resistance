afe46b40a891436fcc5b77927623a38e96740e44
Ariel Davis <ariel.z.davis@icloud.com>
2019-02-28 19:56:35 +0900

    Store name on Party

--- server/party.go
+++ server/party.go
@@ -7,13 +7,15 @@ type PID uint64
 // isolated from all other Parties. When the Party is disbanded, all clients
 // inside will be sent along send.
 type Party struct {
+	name   string         // set by leader
 	send   chan<- *Client // outgoing clients
-	leader *Client        // the leader
+	leader *Client        // if leader leaves, party disbands
 }
 
 // NewParty returns a new Party. The given Client is the leader of the Party.
-func NewParty(send chan<- *Client, leader *Client) *Party {
+func NewParty(name string, send chan<- *Client, leader *Client) *Party {
 	p := &Party{
+		name:   name,
 		send:   send,
 		leader: leader,
 	}
