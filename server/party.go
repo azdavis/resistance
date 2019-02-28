b2e681ececd60df988a2ce7be223c891d63d23b9
Ariel Davis <ariel.z.davis@icloud.com>
2019-02-28 19:52:59 +0900

    Pass send to NewParty

--- server/party.go
+++ server/party.go
@@ -4,14 +4,17 @@ package main
 type PID uint64
 
 // Party represents a group of clients all playing together. A Party is totally
-// isolated from all other Parties.
+// isolated from all other Parties. When the Party is disbanded, all clients
+// inside will be sent along send.
 type Party struct {
-	leader *Client // the leader
+	send   chan<- *Client // outgoing clients
+	leader *Client        // the leader
 }
 
 // NewParty returns a new Party. The given Client is the leader of the Party.
-func NewParty(leader *Client) *Party {
+func NewParty(send chan<- *Client, leader *Client) *Party {
 	p := &Party{
+		send:   send,
 		leader: leader,
 	}
 	return p
