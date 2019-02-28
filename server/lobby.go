3906e7f3a8653c8215a5547082f36bc27e74f01b
Ariel Davis <ariel.z.davis@icloud.com>
2019-02-28 20:58:31 +0900

    Do things with Party

--- server/lobby.go
+++ server/lobby.go
@@ -7,6 +7,7 @@ import (
 // Lobby contains the Clients who are not part of a Party.
 type Lobby struct {
 	recv chan *Client // incoming clients
+	done chan PID     // incoming done parties
 }
 
 // NewLobby returns a new Lobby. It starts a goroutine which never exits.
@@ -14,6 +15,7 @@ func NewLobby() *Lobby {
 	const chLen = 5
 	lb := &Lobby{
 		recv: make(chan *Client, chLen),
+		done: make(chan PID, chLen),
 	}
 	go lb.run()
 	return lb
@@ -22,10 +24,25 @@ func NewLobby() *Lobby {
 // run runs the Lobby.
 func (lb *Lobby) run() {
 	clients := NewClientMap()
+	nextPID := PID(1)
+	parties := make(map[PID]*Party)
+	getPartyInfo := func() []PartyInfo {
+		ret := make([]PartyInfo, 0, len(parties))
+		for pid, party := range parties {
+			ret = append(ret, PartyInfo{PID: pid, Name: party.name})
+		}
+		return ret
+	}
 	for {
 		select {
 		case cl := <-lb.recv:
 			clients.Add(cl)
+		case pid := <-lb.done:
+			cm := parties[pid].clients
+			for cid := range cm.M {
+				clients.Add(cm.Rm(cid))
+			}
+			delete(parties, pid)
 		case cidAc := <-clients.C:
 			id := cidAc.CID
 			switch ac := cidAc.Action.(type) {
@@ -36,8 +53,15 @@ func (lb *Lobby) run() {
 				clients.M[id].name = ac.Name
 				clients.M[id].send <- PartyChoosing{
 					Name:    ac.Name,
-					Parties: []PartyInfo{},
+					Parties: getPartyInfo(),
 				}
+			case PartyChoose:
+				log.Println("PartyChoose", id, ac.PID)
+				parties[ac.PID].recv <- clients.Rm(id)
+			case PartyCreate:
+				log.Println("PartyCreate", id, ac.Name)
+				parties[nextPID] = NewParty(nextPID, ac.Name, clients.Rm(id), lb.done)
+				nextPID++
 			}
 		}
 	}
--- server/party.go
+++ server/party.go
@@ -4,22 +4,62 @@ package main
 type PID uint64
 
 // Party represents a group of clients all playing together. A Party is totally
-// isolated from all other Parties. When the Party is disbanded, all clients
-// inside will be sent along send.
+// isolated from all other Parties.
+//
+// A Party always has at least one client inside. If the leader leaves, the
+// party disbands. New clients who want to join will arrive on recv. However,
+// the party will only accept these new clients if the game has not yet started.
+// The leader decides when to start the game.
+//
+// A Party can disband itself by sending its own PID along done. Once it does
+// this, it should stop modifying itself (i.e., it should exit from run), since
+// the Lobby which contains a pointer to this Party will receive along done and
+// will start cleaning up the party.
 type Party struct {
-	name   string         // set by leader
-	send   chan<- *Client // outgoing clients
-	leader *Client        // if leader leaves, party disbands
+	id      PID          // unique
+	name    string       // set by leader
+	leader  CID          // controls when game starts, can unilaterally disband
+	done    chan PID     // send own id when party disbands
+	recv    chan *Client // incoming clients
+	clients *ClientMap   // clients in the party (includes leader)
 }
 
-// NewParty returns a new Party. The given Client is the leader of the Party.
-func NewParty(name string, send chan<- *Client, leader *Client) *Party {
+// NewParty returns a new Party.
+func NewParty(
+	id PID,
+	name string,
+	leader *Client,
+	done chan PID,
+) *Party {
+	clients := NewClientMap()
+	clients.Add(leader)
 	p := &Party{
-		name:   name,
-		send:   send,
-		leader: leader,
+		id:      id,
+		name:    name,
+		leader:  leader.id,
+		done:    done,
+		recv:    make(chan *Client),
+		clients: clients,
 	}
+	go p.run()
 	return p
 }
 
-func (p *Party) run() {}
+func (p *Party) run() {
+	for {
+		select {
+		case cl := <-p.recv:
+			p.clients.Add(cl)
+		case cidAc := <-p.clients.C:
+			id := cidAc.CID
+			switch cidAc.Action.(type) {
+			case Close:
+				p.clients.Rm(id).Close()
+				if id == p.leader {
+					p.done <- p.id
+					return
+				}
+			}
+		}
+	}
+}
