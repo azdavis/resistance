78b9c2b44b1ce4efc6ceef3f233c5dfbe9a7f00e
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-04 22:50:42 +0900

    Swap

--- client/src/types.ts
+++ client/src/types.ts
@@ -3,6 +3,14 @@ import { Dispatch } from "react";
 export type PID = number;
 export type CID = number;
 
+export type ToServer =
+  | { T: "NameChoose"; Name: string }
+  | { T: "PartyChoose"; PID: PID }
+  | { T: "PartyLeave" }
+  | { T: "PartyCreate" };
+
+export type Send = Dispatch<ToServer>;
+
 export type PartyInfo = {
   PID: PID;
   Leader: string;
@@ -13,17 +21,9 @@ export type ClientInfo = {
   Name: string;
 };
 
-export type State = { T: "Closed" } | ToClient;
-
 export type ToClient =
   | { T: "NameChoosing"; Valid: boolean }
   | { T: "PartyChoosing"; Parties: Array<PartyInfo> }
   | { T: "PartyWaiting"; Self: CID; Leader: CID; Clients: Array<ClientInfo> };
 
-export type ToServer =
-  | { T: "NameChoose"; Name: string }
-  | { T: "PartyChoose"; PID: PID }
-  | { T: "PartyLeave" }
-  | { T: "PartyCreate" };
-
-export type Send = Dispatch<ToServer>;
+export type State = { T: "Closed" } | ToClient;
