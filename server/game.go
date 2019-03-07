00afadf306227451a432baa9de6da69cc2e4a63d
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-07 17:24:15 +0900

    Determine spies

--- server/game.go
+++ server/game.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"log"
+	"math/rand"
 )
 
 func runGame(gid GID, leaderID CID, tx chan<- LobbyMsg, clients *ClientMap) {
+	// about 1/s of the clients will be spies.
+	const s = 4
 	log.Println("enter runGame", gid)
 	defer log.Println("exit runGame", gid)
+
+	cs := clients.ToList()
+
+	isSpy := make(map[CID]bool)
+	for i := len(clients.M) / s; i > 0; /* intentionally empty */ {
+		cid := cs[rand.Intn(len(cs))].CID
+		if !isSpy[cid] {
+			isSpy[cid] = true
+			i--
+		}
+	}
+	log.Println("spies", isSpy)
+
+	for {
+		ac := <-clients.C
+		cid := ac.CID
+		switch ac.ToServer.(type) {
+		case Close:
+			clients.Rm(cid).Close()
+			tx <- LobbyMsg{gid, false, cs}
+			return
+		}
+	}
 }
