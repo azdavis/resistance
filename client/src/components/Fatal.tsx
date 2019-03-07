2fb15732ae780ecb58116e0312ade1aa7e66d705
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-07 21:22:03 +0900

    Explain the fatal error

--- client/src/components/Fatal.tsx
+++ client/src/components/Fatal.tsx
@@ -9,6 +9,10 @@ type Props = {
 export default ({ s, a }: Props): JSX.Element => (
   <div className="Fatal">
     <h1>Fatal error</h1>
+    <p>
+      An occurrence, that the developer of this app did not foresee occurring,
+      occurred. Said occurrence is shown below.
+    </p>
     <pre>{JSON.stringify({ s, a }, null, 2)}</pre>
   </div>
 );
