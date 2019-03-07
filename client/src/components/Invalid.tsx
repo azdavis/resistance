779e7ce2950729991c939a3eda82abc42356c703
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-07 21:13:53 +0900

    Use one <pre>

--- client/src/components/Invalid.tsx
+++ client/src/components/Invalid.tsx
@@ -6,15 +6,10 @@ type Props = {
   a: Action;
 };
 
-const showJSON = (x: any): string => JSON.stringify(x, null, 2);
-
 export default ({ s, a }: Props): JSX.Element => (
   <div className="Invalid">
     <h1>Fatal error</h1>
     <p>This is probably the developer's fault.</p>
-    <p>Previous state:</p>
-    <pre>{showJSON(s)}</pre>
-    <p>Action:</p>
-    <pre>{showJSON(a)}</pre>
+    <pre>{JSON.stringify({ s, a }, null, 2)}</pre>
   </div>
 );
