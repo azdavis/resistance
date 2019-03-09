09354b8d579bb54b9c0e96f6346f5b05c22cc8f7
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-09 13:39:32 +0900

    Use Clear

--- server/game.go
+++ server/game.go
@@ -42,7 +42,7 @@ func runGame(gid GID, leaderID CID, tx chan<- LobbyMsg, clients *ClientMap) {
 		switch ac.ToServer.(type) {
 		case Close:
 			clients.Rm(cid).Close()
-			tx <- LobbyMsg{gid, false, clients.ToList()}
+			tx <- LobbyMsg{gid, false, clients.Clear()}
 			return
 		}
 	}
