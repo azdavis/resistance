94b103ff793cd2a3fecb5a2ddef9e70d96d9fee1
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-13 18:32:36 +0900

    Rm empty Props

--- client/src/components/MissionWaiter.tsx
+++ client/src/components/MissionWaiter.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-type Props = {};
-
-export default ({  }: Props) => (
+export default () => (
   <div className="MissionWaiter">
     <h1>Mission in progress</h1>
     <p>The mission is currently occurring.</p>
