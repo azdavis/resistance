a570f47f42f46b28bceeab681d1c107d6084a037
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-17 01:23:00 +0900

    Show the role up front

--- client/src/components/states/RoleViewer.tsx
+++ client/src/components/states/RoleViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { D } from "../../types";
 import Button from "../Button";
 
@@ -8,12 +8,10 @@ type Props = {
 };
 
 export default ({ d, isSpy }: Props) => {
-  const [show, setShow] = useState(false);
-  const value = show ? `You ${isSpy ? "are" : "are not"} a spy` : "View role";
   return (
     <div className="RoleViewer">
       <h1>Role</h1>
-      <Button value={value} onClick={() => setShow(!show)} />
+      <p>You {isSpy ? "are" : "are not"} a spy.</p>
       <Button value="Continue" onClick={() => d({ t: "AckRole" })} />
     </div>
   );
