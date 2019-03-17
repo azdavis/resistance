96ff38a16a488a97cba05a5024445ffd36cee0b2
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-17 23:08:48 +0900

    Add ButtonLink

---
+++ client/src/components/basic/ButtonLink.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ButtonLink.css";
+import "./Button.css";
+import "./Truncated.css";
+
+type Props = {
+  value: string;
+  href: string;
+};
+
+export default ({ value, href }: Props) => (
+  <a href={href} className="ButtonLink Button Truncated">
+    {value}
+  </a>
+);
