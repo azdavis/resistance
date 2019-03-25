7347182be3f79ff0cad6f4ac1cb9f13e955b45eb
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-25 22:58:08 +0900

    Add Server

--- server/main.go
+++ server/main.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"time"
 )
-
-func newServer() *http.Server {
-	txLobbyMap := make(chan ToLobbyMap, 3)
-	txWelcomer := make(chan *Client, 3)
-	go runLobbyMap(txLobbyMap)
-	go runWelcomer(txLobbyMap, txWelcomer)
-	s := &http.Server{
-		Handler:      NewHub(txWelcomer),
-		Addr:         ":8080",
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-	}
-	return s
-}
 
 func main() {
 	log.Println("start")
-	log.Fatal(newServer().ListenAndServe())
+	log.Fatal(NewServer().HTTPServer.ListenAndServe())
 }
---
+++ server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+// Server runs the backend.
+type Server struct {
+	HTTPServer *http.Server
+}
+
+// NewServer returns a new Server.
+func NewServer() *Server {
+	txLobbyMap := make(chan ToLobbyMap, 3)
+	txWelcomer := make(chan *Client, 3)
+	go runLobbyMap(txLobbyMap)
+	go runWelcomer(txLobbyMap, txWelcomer)
+	hs := &http.Server{
+		Handler:      NewHub(txWelcomer),
+		Addr:         ":8080",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	s := &Server{hs}
+	return s
+}
+
+// Close shuts down the Server. It should only be called once.
+func (s *Server) Close() {
+	s.HTTPServer.Shutdown(context.Background())
+}
