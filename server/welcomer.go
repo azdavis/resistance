cbadb22dbedfbe285b629a6225ee35f166a0c0f9
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-25 23:17:48 +0900

    Use fewer lines

--- server/welcomer.go
+++ server/welcomer.go
@@ -11,11 +11,7 @@ func validName(s string) bool {
 	return s != "" && len(s) <= maxLen && validNameRE.Match([]byte(s))
 }
 
-func runWelcomer(
-	tx chan<- ToLobbyMap,
-	rx <-chan *Client,
-	q <-chan struct{},
-) {
+func runWelcomer(tx chan<- ToLobbyMap, rx <-chan *Client, q <-chan struct{}) {
 	clients := NewClientMap()
 	for {
 		select {
