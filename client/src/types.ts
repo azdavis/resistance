9a3fead638cc58ae7eecc27a2a0c293bebf9beaf
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-25 18:52:41 +0900

    Use caps

--- client/src/types.ts
+++ client/src/types.ts
@@ -6,7 +6,7 @@ export type CID = number;
 
 type ToServer =
   | { t: "Connect" }
-  | { t: "Reconnect"; me: CID; gid: GID }
+  | { t: "Reconnect"; Me: CID; GID: GID }
   | { t: "NameChoose"; Name: string }
   | { t: "LobbyChoose"; GID: GID }
   | { t: "LobbyLeave" }
