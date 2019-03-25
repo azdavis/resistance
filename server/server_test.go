b4b48c3b72b5453b1070dc4e77e16d83e2ddb5d7
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-26 01:28:36 +0900

    Add some testing helpers

--- server/server_test.go
+++ server/server_test.go
@@ -6,6 +6,162 @@ import (
 	"time"
 )
 
+// testClient //////////////////////////////////////////////////////////////////
+
+type testClient struct {
+	cl  *Client
+	req chan struct{}
+	res chan ToClient
+}
+
+func newTestClient() *testClient {
+	cl := &Client{
+		CID:  0,
+		Name: "",
+		tx:   make(chan ToClient, ClientChLen),
+		rx:   make(chan ToServer, ClientChLen),
+		conn: nil,
+	}
+	tc := &testClient{cl, make(chan struct{}), make(chan ToClient)}
+	go tc.doTestTx()
+	return tc
+}
+
+func (tc *testClient) doTestTx() {
+	// invariant: for all i, next <= i < have, ms[i] exists
+	next := uint(0)
+	have := uint(0)
+	// invariant: for all i, i < want, ms[i] was requested
+	want := uint(0)
+	ms := make(map[uint]ToClient)
+	for {
+		select {
+		case m := <-tc.cl.tx:
+			ms[have] = m
+			have++
+		case <-tc.req:
+			want++
+		}
+		for next < have && next < want {
+			tc.res <- ms[next]
+			delete(ms, next)
+			next++
+		}
+	}
+}
+
+func (tc *testClient) send(m ToServer) {
+	tc.cl.rx <- m
+}
+
+func (tc *testClient) recv() ToClient {
+	tc.req <- struct{}{}
+	return <-tc.res
+}
+
+func (tc *testClient) recvSetMe(t *testing.T) SetMe {
+	x := tc.recv()
+	y, ok := x.(SetMe)
+	if !ok {
+		t.Fatal("response was not SetMe")
+	}
+	return y
+}
+
+func (tc *testClient) recvNameReject(t *testing.T) NameReject {
+	x := tc.recv()
+	y, ok := x.(NameReject)
+	if !ok {
+		t.Fatal("response was not NameReject")
+	}
+	return y
+}
+
+func (tc *testClient) recvLobbyChoices(t *testing.T) LobbyChoices {
+	x := tc.recv()
+	y, ok := x.(LobbyChoices)
+	if !ok {
+		t.Fatal("response was not LobbyChoices")
+	}
+	if y.Lobbies == nil {
+		t.Fatal("Lobbies was nil")
+	}
+	return y
+}
+
+func (tc *testClient) recvCurrentLobby(t *testing.T) CurrentLobby {
+	x := tc.recv()
+	y, ok := x.(CurrentLobby)
+	if !ok {
+		t.Fatal("response was not CurrentLobby")
+	}
+	if y.Clients == nil {
+		t.Fatal("Clients was nil")
+	}
+	return y
+}
+
+func (tc *testClient) recvCurrentGame(t *testing.T) CurrentGame {
+	x := tc.recv()
+	y, ok := x.(CurrentGame)
+	if !ok {
+		t.Fatal("response was not CurrentGame")
+	}
+	if !(0 <= y.ResPts && y.ResPts < MaxPts) {
+		t.Fatal("ResPts not in range", y.ResPts)
+	}
+	if !(0 <= y.SpyPts && y.SpyPts < MaxPts) {
+		t.Fatal("SpyPts not in range", y.SpyPts)
+	}
+	if y.Members != nil && len(y.Members) != y.NumMembers {
+		t.Fatal("number of members differ", len(y.Members), y.NumMembers)
+	}
+	if y.Active && y.Members == nil {
+		t.Fatal("Members nil when active")
+	}
+	return y
+}
+
+func (tc *testClient) recvEndGame(t *testing.T) EndGame {
+	x := tc.recv()
+	y, ok := x.(EndGame)
+	if !ok {
+		t.Fatal("response was not EndGame")
+	}
+	if !(0 <= y.ResPts && y.ResPts < MaxPts) {
+		t.Fatal("ResPts not in range", y.ResPts)
+	}
+	if !(0 <= y.SpyPts && y.SpyPts < MaxPts) {
+		t.Fatal("SpyPts not in range", y.SpyPts)
+	}
+	if y.ResPts == MaxPts && y.SpyPts == MaxPts {
+		t.Fatal("both ResPts and SpyPts are MaxPts")
+	}
+	if y.ResPts != MaxPts && y.SpyPts != MaxPts {
+		t.Fatal("neither ResPts nor SpyPts are MaxPts")
+	}
+	return y
+}
+
+// addClient ///////////////////////////////////////////////////////////////////
+
+func (s *Server) addClient(t *testing.T) *testClient {
+	tc := newTestClient()
+	tc.send(Connect{})
+	s.C <- tc.cl
+	m := tc.recv()
+	sm, ok := m.(SetMe)
+	if !ok {
+		t.Fatal("response was not SetMe")
+	}
+	if sm.Me != tc.cl.CID {
+		t.Fatal("SetMe CIDs differ", sm.Me, tc.cl.CID)
+	}
+	return tc
+}
+
+// Tests ///////////////////////////////////////////////////////////////////////
+
 func TestBasicNumGoroutine(t *testing.T) {
 	before := runtime.NumGoroutine()
 	s := NewServer()
