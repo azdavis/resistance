46a1c73de2c5c14641f8b78963da918a392aab75
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-29 17:59:30 +0900

    Explain why s unnecessary

--- client/src/hooks/useTriggerEffect.ts
+++ client/src/hooks/useTriggerEffect.ts
@@ -4,7 +4,9 @@ const flip: Reducer<boolean, void> = (s, _) => !s;
 
 export default (effect: EC, deps?: ReadonlyArray<any>): (() => void) => {
   const [s, d] = useReducer(flip, true);
-  // HACK to get the effect to re-run when we want to.
+  // HACK to get the effect to re-run when we want to. if deps was undefined,
+  // then it gets run on every render. calling d will trigger a re-render, so no
+  // need to include `s` in the array of deps.
   useEffect(effect, deps ? deps.concat([s]) : deps);
   // HACK there's no need to explicitly pass `undefined` to `d`, so we can lie
   // to the type system.
