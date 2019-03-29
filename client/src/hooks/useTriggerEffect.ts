923548575bfb01972fcf169b7e522bd1cf03720f
Ariel Davis <ariel.z.davis@icloud.com>
2019-03-29 17:59:22 +0900

    Move some types

--- client/src/hooks/useTriggerEffect.ts
+++ client/src/hooks/useTriggerEffect.ts
@@ -1,12 +1,12 @@
-import { useReducer, useEffect, Reducer, EffectCallback } from "react";
+import { useReducer, useEffect, Reducer, EffectCallback as EC } from "react";
 
 const flip: Reducer<boolean, void> = (s, _) => !s;
 
-export default (effect: EffectCallback, deps?: ReadonlyArray<any>) => {
+export default (effect: EC, deps?: ReadonlyArray<any>): (() => void) => {
   const [s, d] = useReducer(flip, true);
   // HACK to get the effect to re-run when we want to.
   useEffect(effect, deps ? deps.concat([s]) : deps);
   // HACK there's no need to explicitly pass `undefined` to `d`, so we can lie
   // to the type system.
-  return (d as any) as (() => void);
+  return d as any;
 };
