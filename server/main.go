94ac2780db4cbe5f5cc2e4a67fc450d8615399d1
Ariel Davis <ariel.z.davis@icloud.com>
2019-04-01 20:14:50 +0900

    Use getPort

--- server/main.go
+++ server/main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+	return port
+}
 
 func main() {
 	fmt.Println("start")
 	hs := &http.Server{
 		Handler:      NewHub(NewServer().C),
-		Addr:         ":8080",
+		Addr:         ":" + getPort(),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
