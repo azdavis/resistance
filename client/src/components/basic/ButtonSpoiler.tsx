11c1ee0012bb4c4312a48d4b566f43b38f3f5315
Ariel Davis <ariel.z.davis@icloud.com>
2019-04-02 21:01:50 +0900

    Add ButtonSpoiler

---
+++ client/src/components/basic/ButtonSpoiler.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useRef } from "react";
+import Button from "./Button";
+
+type Props = {
+  view: string;
+  spoil: string;
+  delay?: number;
+};
+
+export default ({ view, spoil, delay = 1000 }: Props) => {
+  const [visible, setVisible] = useState(false);
+  const id = useRef<NodeJS.Timeout | null>(null);
+  return (
+    <Button
+      value={visible ? spoil : view}
+      onClick={() => {
+        setVisible(true);
+        if (id.current !== null) {
+          clearInterval(id.current);
+        }
+        id.current = setInterval(() => setVisible(false), delay);
+      }}
+    />
+  );
+};
