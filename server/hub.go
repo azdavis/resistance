74ceaddf657e9be99ed672ca41ab104ff1117488
Ariel Davis <ariel.z.davis@icloud.com>
2019-06-14 02:03:59 -0700

    Give HTTP OK on /

--- server/hub.go
+++ server/hub.go
@@ -49,6 +49,10 @@ func checkOrigin(r *http.Request) bool {
 // ServeHTTP tries to upgrade the (w, r) pair into a websocket connection. If it
 // is successful, it makes a new Client with a fresh CID and sends it along tx.
 func (h *Hub) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	if r.URL.Path != "/ws" {
 		http.NotFound(w, r)
 		return
