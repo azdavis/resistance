7d19ea68cbf9c1429ad5fcb997a9d2826f7ca121
Ariel Davis <ariel.z.davis@icloud.com>
2019-06-15 16:24:41 -0700

    Combine some paragraphs

--- client/src/translations/en.ts
+++ client/src/translations/en.ts
@@ -38,19 +38,19 @@ const en: Translation = {
   viewCode: "View source code",
   howTo: [
     `Groups of at least ${minN} and at most ${maxN} players may play.`,
-    "Some players are spies. The rest are members of the resistance.",
-    `The first of the spies and resistance to get ${maxPts} wins the game.`,
+    "Some players are spies. The rest are members of the resistance. " +
+      `The first of the spies and resistance to get ${maxPts} wins the game.`,
     "The game is played in rounds. " +
       "In every round of the game, a captain is chosen. " +
-      "The captain chooses the mission members for this round.",
-    "When the captain has finished choosing, " +
+      "The captain chooses the mission members for this round. " +
+      "When the captain has finished choosing, " +
       "all players vote on whether the mission occurs.",
-    "If the mission does not occur, the next round is started.",
-    "If too many missions do not occur in a row, the spies get 1 point.",
-    "If the mission does occur, " +
+    "If the mission does not occur, the next round is started. " +
+      "If too many missions do not occur in a row, the spies get 1 point. " +
+      "If the mission does occur, " +
       "the members of the mission vote on whether the mission succeeds.",
-    "If the mission succeeds, the resistance gets 1 point.",
-    "If the mission fails, the spies get 1 point.",
+    "If the mission succeeds, the resistance gets 1 point. " +
+      "If the mission fails, the spies get 1 point.",
   ],
 };
 
